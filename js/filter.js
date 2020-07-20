0758153fbf3c0f9d4ea3ab6c21e1cb73d010b7a5
alchepak <alchepak@gmail.com>
2020-07-21 00:00:45 +0800

    Переносит обработчик изменения фильтра в модуль filter

--- js/filter.js
+++ js/filter.js
@@ -2,11 +2,16 @@
 
 (function () {
   var DEFAULT_VALUE = 'any';
+  var filtersForm = document.querySelector('.map__filters');
   var housingType = document.querySelector('#housing-type');
 
   var filterByHousingType = function (it) {
     return it.offer.type === housingType.value;
   };
+
+  filtersForm.addEventListener('change', function () {
+    window.data.updateAdverts();
+  });
 
   window.filter = function (data) {
     var items = data.slice();
