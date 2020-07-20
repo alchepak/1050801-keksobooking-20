2bd6d4d5e6b659b9d636dfcacaad8595b72543f4
alchepak <alchepak@gmail.com>
2020-07-21 00:10:04 +0800

    Изменяет модули и страницу в соответствии с последними коммитами

--- js/main.js
+++ js/main.js
@@ -1,37 +1,5 @@
 'use strict';
 
 (function () {
-  var map = document.querySelector('.map');
-  var mainPin = document.querySelector('.map__pin--main');
-  var adForm = document.querySelector('.ad-form');
-  var filtersForm = document.querySelector('.map__filters');
-
-  var activatePage = function () {
-    if (map.classList.contains('map--faded')) {
-      map.classList.remove('map--faded');
-      adForm.classList.remove('ad-form--disabled');
-
-      window.form.changeFormInputsState(adForm, false);
-      window.form.setCurrentAddress(true);
-      window.validation.checkCapacityValue();
-
-      window.data.getAdverts();
-    }
-  };
-
-  mainPin.addEventListener('mousedown', function (evt) {
-    window.util.isMouseDown(evt, activatePage);
-  });
-
-  mainPin.addEventListener('keydown', function (evt) {
-    window.util.isEnterPress(evt, activatePage);
-  });
-
-  filtersForm.addEventListener('change', function () {
-    window.data.updateAdverts();
-  });
-
-  window.form.changeFormInputsState(adForm, true);
-  window.form.changeFormInputsState(filtersForm, true);
-  window.form.setCurrentAddress(false);
+  window.page.reset();
 })();
